41a6b75fc8e6908cd6354baeedb0e0a1141c35b6
NTh-Hai <thehai318@gmail.com>
2023-02-28 07:08:20 +0700

    tweaking layout

--- src/app/(server-side-rendering)/dynamic/MovieCard.tsx
+++ src/app/(server-side-rendering)/dynamic/MovieCard.tsx
@@ -10,17 +10,19 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
       className="group mx-auto flex h-[22.5rem] w-[15rem] flex-col justify-end overflow-hidden rounded-md shadow-black drop-shadow-2xl duration-500"
       style={{ backgroundImage: `url(${posterURL})`, backgroundSize: "cover" }}
     >
-      <div className="mt-8 bg-gradient-to-t from-black via-black px-3 text-slate-200 transition-all duration-500 group-focus-within:max-h-96  group-focus-within:duration-75 group-hover:max-h-96 sm:max-h-0">
+      <div className="mt-8 bg-gradient-to-t from-black via-black px-3 text-slate-200 transition-all duration-500  ">
         <p className="mt-9 mb-2 font-bold uppercase drop-shadow-xl ">
           {movie.title}
         </p>
-        <p className="my-2 text-[0.75rem] italic">{movie.release_date}</p>
-        <p className="my-2 text-[0.75rem] italic">{movie.overview}</p>
-        <Link href={`/dynamic/movie/${movie.id}`}>
-          <button className="mb-3 rounded-md bg-red-900 px-2 py-1">
-            Details
-          </button>
-        </Link>
+        <div className="duration-500 group-focus-within:max-h-96 group-focus-within:duration-75 group-hover:max-h-96  sm:max-h-0">
+          <p className="my-2 text-[0.75rem] italic">{movie.release_date}</p>
+          <p className="my-2 text-[0.75rem] italic">{movie.overview}</p>
+          <Link href={`/dynamic/movie/${movie.id}`}>
+            <button className="mb-3 rounded-md bg-red-900 px-2 py-1">
+              Details
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
